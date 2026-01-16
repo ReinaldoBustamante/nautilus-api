9c75d721100c4b7b1071d5da6f2b0666283ad678
Reinaldo Bustamante <reinaldo.bustamante026@gmail.com>
2026-01-15 21:55:38 -0300

    refactor(db): changed the database structure to improve the design

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,24 +4,13 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 
-import { UserModule } from './user/user.module';
-import { ServiceModule } from './service/service.module';
-import { SpecializationModule } from './specialization/specialization.module';
-import { DoctorModule } from './doctor/doctor.module';
-import { PatientModule } from './patient/patient.module';
-
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: '.env',
     }),
-    SpecializationModule,
     AuthModule,
-    UserModule,
-    ServiceModule,
-    DoctorModule,
-    PatientModule
   ],
   controllers: [],
   providers: [],
--- src/specialization/dtos/createSpecialization.dto.ts
+++
@@ -1,9 +0,0 @@
-import { Transform } from "class-transformer";
-import { IsNotEmpty, IsString, Min } from "class-validator";
-
-export class CreateSpecializationDto {
-    @IsNotEmpty()
-    @IsString()
-    @Transform(({ value }) => value?.toLowerCase())
-    name: string
-}
