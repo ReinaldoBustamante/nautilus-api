7413dfa329fafe41fd919d25a2aafa8100a605a5
reinaldo bustamante <reinaldo.bustamante026@gmail.com>
2026-01-19 12:30:33 -0300

    feat: finish users endpoints

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Patch, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, UseGuards } from '@nestjs/common';
 import { user_role_type } from 'generated/prisma/enums';
 import { Roles } from 'src/common/decorators/roles.decorator';
 import { AuthGuard } from 'src/common/guards/auth/auth.guard';
@@ -7,32 +7,29 @@ import { UsersService } from './users.service';
 import { CreateUserDto } from './dtos/createUserDto';
 import { UpdateUserDto } from './dtos/updateUserDto';
 
-
+@UseGuards(AuthGuard, RolesGuard)
+@Roles(user_role_type.admin)
 @Controller('users')
 export class UsersController {
     constructor(private readonly usersService: UsersService) { }
 
-    @UseGuards(AuthGuard, RolesGuard)
-    @Roles(user_role_type.admin)
     @Get('')
     getUsers() {
         return this.usersService.findAll()
     }
 
-    @UseGuards(AuthGuard, RolesGuard)
-    @Roles(user_role_type.admin)
     @Post('')
     createUser(@Body() payload: CreateUserDto) {
         return this.usersService.create(payload)
     }
 
-    @UseGuards(AuthGuard, RolesGuard)
-    @Roles(user_role_type.admin)
     @Patch(':id')
     updateUser(@Body() payload: UpdateUserDto, @Param('id') id: string){
         return this.usersService.update(payload, id)
     }
-
- 
-
+    
+    @Delete(':id')
+    deleteUser(@Param('id') id: string){
+        return this.usersService.delete(id)
+    }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,41 +1,69 @@
-import { ConflictException, Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma.service';
 import { CreateUserDto } from './dtos/createUserDto';
 import { UpdateUserDto } from './dtos/updateUserDto';
 import { BcryptAdapter } from 'src/auth/adapters/bcrypt.adapter';
 
+
+const USER_SELECT_FIELDS = {
+    id: true,
+    email: true,
+    user_role: true,
+    user_status: true,
+    created_at: true,
+    updated_at: true,
+    deleted_at: true,
+};
+
 @Injectable()
 export class UsersService {
-    constructor(private readonly prisma: PrismaService, private bcryptAdapter: BcryptAdapter){}
-
-    async findAll(){
+    constructor(private readonly prisma: PrismaService, private bcryptAdapter: BcryptAdapter) { }
+    
+    async findAll() {
         return await this.prisma.user.findMany({
-            select: {id: true, email: true, user_role: true, user_status: true, created_at: true, updated_at: true, deleted_at: true}
+            select: USER_SELECT_FIELDS
         });
     }
 
-    async create(payload: CreateUserDto){
+    async create(payload: CreateUserDto) {
         try {
-            return await this.prisma.user.create({ 
-                data: {...payload, password: await this.bcryptAdapter.encryptPassword(payload.password)},
-                select: {id: true, email: true, user_role: true, user_status: true, created_at: true, updated_at: true, deleted_at: true}
+            return await this.prisma.user.create({
+                data: { ...payload, password: await this.bcryptAdapter.encryptPassword(payload.password) },
+                select: USER_SELECT_FIELDS
             })
         } catch (err) {
-            if (err.code === 'P2002') throw new ConflictException('user already exist')
+            if (err.code === 'P2002') throw new ConflictException('User already exist')
+            throw err
         }
     }
 
-    async update(payload: UpdateUserDto, id: string){
-        return await this.prisma.user.update({
-            data: payload,
-            where: { id }
-        })
+    async update(payload: UpdateUserDto, id: string) {
+        try {
+            return await this.prisma.user.update({
+                data: {
+                    ...payload,
+                    updated_at: new Date()
+                },
+                where: { id },
+                select: USER_SELECT_FIELDS
+            })
+        } catch (err) {
+            if (err.code === 'P2025') throw new NotFoundException('Resource not found')
+            if (err.code === 'P2002') throw new ConflictException('Email already in use');
+            throw err
+        }
     }
 
-    async delete(id: string){
-        return await this.prisma.user.update({
-            data: { deleted_at: new Date() },
-            where: { id }
-        })
+    async delete(id: string) {
+        try {
+            return await this.prisma.user.update({
+                data: { deleted_at: new Date(), user_status: 'deleted' },
+                select: USER_SELECT_FIELDS,
+                where: { id }
+            })
+        } catch (err) {
+            if (err.code === 'P2025') throw new NotFoundException('Resource not found')
+            throw err
+        }
     }
 }
