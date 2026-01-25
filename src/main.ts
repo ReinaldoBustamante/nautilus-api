287f95fa9fdc1a913fec91fb018647a3c7754d8f
reinaldo bustamante <reinaldo.bustamante026@gmail.com>
2026-01-25 12:44:24 -0300

    fix: Add cors configuration

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,15 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true
   }));
+  app.enableCors({
+    origin: [
+      //frontend url
+      'http://localhost:5173',
+      'http://192.168.1.9:5173' 
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
