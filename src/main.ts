e5e3c54c27b51e798809f7af87e6bd4e15bf423f
reinaldo bustamante <reinaldo.bustamante026@gmail.com>
2026-02-09 17:49:21 -0300

    refactor: add cors_origin at environment var

--- src/main.ts
+++ src/main.ts
@@ -8,13 +8,7 @@ async function bootstrap() {
     whitelist: true
   }));
   app.enableCors({
-    origin: [
-      //frontend url
-      'http://localhost:5173',
-      'http://192.168.1.9:5173',
-      'http://192.168.1.9:4173',
-      'https://musical-torte-08040e.netlify.app'
-    ],
+    origin: process.env.CORS_ORIGIN,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
