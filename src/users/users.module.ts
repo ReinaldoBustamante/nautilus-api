97258308667f7ff233a8589d7fd7a946a610e1d4
reinaldo bustamante <reinaldo.bustamante026@gmail.com>
2026-03-12 14:54:03 -0300

    feat: configure endpoint to manage custom errors

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,9 +3,10 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { PrismaService } from 'src/prisma.service';
 import { JWTAdapter } from 'src/common/adapters/jwt.adapter';
+import { BcryptAdapter } from 'src/common/adapters/bcrypt.adapter';
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService, PrismaService, JWTAdapter],
+  providers: [UsersService, PrismaService, JWTAdapter, BcryptAdapter],
 })
 export class UsersModule {}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,34 +2,67 @@ import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { PrismaService } from 'src/prisma.service';
+import { prismaErrorMap } from 'src/common/errors/prisma.errors';
+import { BcryptAdapter } from 'src/common/adapters/bcrypt.adapter';
+
+const USER_SELECT_FIELDS = {
+  id: true,
+  email: true,
+  role: true,
+  status: true,
+  created_at: true,
+  updated_at: true,
+};
 
 @Injectable()
 export class UsersService {
   constructor(
     private readonly prismaService: PrismaService,
+    private readonly bcryptAdapter: BcryptAdapter
   ) { }
 
   async create(createUserDto: CreateUserDto) {
-    const user = await this.prismaService.user.create({
-      data: createUserDto
-    })
-
-    return user
+    const { password, ...restUser } = createUserDto
+    try {
+      const user = await this.prismaService.user.create({
+        data: {
+          ...restUser,
+          password: await this.bcryptAdapter.encryptPassword(password)
+        }
+      })
+      const { password: passwordHashed, ...userCreate } = user
+      return userCreate
+    } catch (err) {
+      const handler = prismaErrorMap[err.code]
+      if (handler) throw handler()
+    }
   }
 
   async findAll() {
-    const users = await this.prismaService.user.findMany()
-    return users
+    try {
+      const users = await this.prismaService.user.findMany({
+        select: USER_SELECT_FIELDS
+      })
+      return users
+    } catch (err) {
+      const handler = prismaErrorMap[err.code]
+      if (handler) throw handler()
+    }
   }
 
   async update(id: string, updateUserDto: UpdateUserDto) {
-    const user = await this.prismaService.user.update({
-      where: {
-        id: id
-      },
-      data: updateUserDto
-    })
-    return user
+    try {
+      const user = await this.prismaService.user.update({
+        where: {
+          id: id
+        },
+        data: updateUserDto
+      })
+      return user
+    } catch (err) {
+      const handler = prismaErrorMap[err.code]
+      if (handler) throw handler()
+    }
   }
 
 }
